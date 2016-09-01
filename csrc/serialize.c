2a9a5915083031d4acaa551c30e3355f66e62f6b
Eric Hoffman <eric@kodowa.com>
2016-09-01 14:17:13 -0700

    start serializer

--- csrc/serialize.c
+++ csrc/serialize.c
@@ -1,4 +1,3 @@
-
 // the serialization typespace
 #define uuid_bits 0x80
 #define uuid_mask 0x7f
@@ -9,24 +8,75 @@
 #define float_bits 0x13
 #define float_mask 0x00
 
+/*
+ 1xxxxxxx uuid   // remember to steal this bit
+ 01xxxxxx string // followed by length encoded as - 0xxxx immedate (32 bytes :()
+ 001xxxxx ??
+ 00010000 bigdec // not a thing
+ 00000    constant space - see below
+ 00000100 version zero // ?
+ 00000011 triple marker // doesn't need to be a whole btye
+ 00000010 float64 // - unless we do the denorm trick, then this guy would likely be 00000000
+ 00000001 true
+ 00000000 false
+*/
+o
+#define version_zero 0x08
+#define triple_marker 0x03
+#define float64_prelude 0x02
+#define true 0x01
+#define false 0x0
 
-// 1 x x x x x x x uuid
-// 0 1 x x x x x x uuid
-// 0 0 1 x x x x x string
-// 0 0 0 1 0 0 0 0 bigdec
-// 0 0 0 1 0 0 0 1 float64
-// 0 0 0 1 0 0 1 1 float64
-// 0 0 0 0 0 0 0 1 true
-// 0 0 0 0 0 0 0 0 false
-//["0xxxxxxx"  decode-uuid]
-//["111xxxxx"  decode-bigdec]
-//["1010xxxx"  decode-vector]
-//["1001xxxx"  decode-string]
-//["10001010"  decode-five-tuple]
-//["10001011"  version1]
-//["10001001"  true]
-//["10001000"  false]])]
-
-void serialize(buffer dest, value v)
+void serialize_value(buffer dest, value v)
 {
+
+    switch(type_of(v)) {
+    case register_space:
+    case estring_space:
+    case float_space:
+    case uuid_space:
+    }
+}
+
+void serialize_edb(buffer dest, edb db)
+{
+    edb_foreach(db, e, a, v, m) {
+        buffer_write_byte(dest);
+    }
+}
+
+
+typedef struct deserialize {
+    closure(handler, value);
+    buffer partial;
+    unsigned char basetype;
+    unsigned int length;
+    unsigned long target;
+} *deserialize;
+
+static value constant_translation(byte z)
+{
+}
+
+CONTINUATION_1_1(deserialize d, buffer b);
+static void deserialize_input(deserialize d, buffer b)
+{
+    buffer intermediate;
+    unsigned long target_type;
+    if (length == 0) {
+        switch(target_type) {
+        case uuid_space:
+            apply(intern_uuid(bref(d->partial, 0)));
+
+        }
+        buffer_clear(d->partial);
+    }
+
+}
+
+
+buffer_handler allocate_deserialize(heap h, closure(handler,value))
+{
+
+
 }
